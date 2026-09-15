74e021a347bcee5ff342ee1212af0fcebf6c6a38
Lukas Trillat <lstrillat@gmail.com>
2026-09-14 23:05:17 -0300

    refactor(incident): replace Class1 with proper AddInfrastructure extension method

--- src/Services/Incident/Incident.Infrastructure/DependencyInjection.cs
+++ src/Services/Incident/Incident.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,13 @@
-﻿namespace Incident.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class Class1
+namespace Incident.Infrastructure;
+
+public static class DependencyInjection
 {
-
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // TODO: Register DbContext and Repositories here
+        return services;
+    }
 }
