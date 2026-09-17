383338d07118a7fc160facfd72b0324e1eaed9e2
Nikoooo <ncayuman2025@alu.uct.cl>
2026-09-17 18:11:36 -0300

    feat(campus): configure entity relationships and DbSets in CampusDbContext

--- src/Services/Campus/Campus.Infraestructure/Persistence/CampusDbContext.cs
+++ src/Services/Campus/Campus.Infraestructure/Persistence/CampusDbContext.cs
@@ -1,4 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Campus.Domain.Entities;
+using Campus.Domain.ValueObjects;
+using CampusEntity = Campus.Domain.Entities.Campus;
 
 namespace Campus.Infraestructure.Persistence
 {
@@ -8,6 +11,58 @@ namespace Campus.Infraestructure.Persistence
         {
         }
 
-        // Aquí agregaremos luego las entidades (CampusEntity, BuildingEntity, etc.)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CampusEntity>()
+                .HasMany(c => c.Buildings)
+                .WithOne(b => b.Campus)
+                .HasForeignKey(b => b.CampusId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<CampusEntity>()
+                .HasMany(c => c.Structures)
+                .WithOne(s => s.Campus)
+                .HasForeignKey(s => s.CampusId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<CampusEntity>()
+                .OwnsOne(c => c.Coordinates);
+
+
+            modelBuilder.Entity<Building>()
+                .HasMany(b => b.Rooms)
+                .WithOne(r => r.Building)
+                .HasForeignKey(r => r.BuildingId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Building>()
+                .OwnsOne(b => b.Coordinates);
+
+
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Structures)
+                .WithOne(s => s.Category)
+                .HasForeignKey(s => s.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Rooms)
+                .WithOne(r => r.Category)
+                .HasForeignKey(r => r.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+            modelBuilder.Entity<Structure>()
+                .OwnsOne(s => s.Coordinates);
+        }
+
+        public DbSet<CampusEntity> Campuses { get; set; }
+            public DbSet<Building> Buildings { get; set; }
+            public DbSet<Room> Rooms { get; set; }
+            public DbSet<Structure> Structures { get; set; }
+            public DbSet<Category> Categories { get; set; }
+        
     }
 }
