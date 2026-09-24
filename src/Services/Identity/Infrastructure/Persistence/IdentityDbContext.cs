635a66e2251643aafd2409d00f0cc225ec44ab24
notKechai <kronokechai@gmail.com>
2026-09-24 01:19:37 -0300

    fix: actual merge with taller-2 and restored role table mapping.

--- src/Services/Identity/Infrastructure/Persistence/IdentityDbContext.cs
+++ src/Services/Identity/Infrastructure/Persistence/IdentityDbContext.cs
@@ -16,5 +16,6 @@ public class IdentityDbContext : IdentityDbContext<User, Role, string>
         base.OnModelCreating(builder);
 
         builder.Entity<User>().ToTable("Users");
+        builder.Entity<Role>().ToTable("Identidad_Rol");
     }
 }
