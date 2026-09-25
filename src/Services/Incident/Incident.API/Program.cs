d46dc8edafca9af02694f0a280b238f9415974b7
Lukas Trillat <lstrillat@gmail.com>
2026-09-25 13:09:33 -0300

    feat(incident): add swagger to API for local testing

--- src/Services/Incident/Incident.API/Program.cs
+++ src/Services/Incident/Incident.API/Program.cs
@@ -7,10 +7,17 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddProblemDetails();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.UseExceptionHandler();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapControllers();
 
@@ -19,4 +26,5 @@ app.Run();
 public partial class Program
 {
 }
+
 
