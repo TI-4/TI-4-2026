76e12d9f94d750fc2c38204788ee3f1254183273
sofianoguera <sofiarodrigueznog@gmail.com>
2026-09-25 10:56:46 -0300

    feat(schedule): add migration for office hour structure reference

---
+++ src/Services/Schedule/Schedule.Infrastructure/Migrations/20260925135554_AddOfficeHourStructureReference.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Schedule.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOfficeHourStructureReference : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "StructureRefId",
+                table: "OfficeHours",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OfficeHours_StructureRefId_DayOfWeek",
+                table: "OfficeHours",
+                columns: new[] { "StructureRefId", "DayOfWeek" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OfficeHours_StructureRefId_DayOfWeek",
+                table: "OfficeHours");
+
+            migrationBuilder.DropColumn(
+                name: "StructureRefId",
+                table: "OfficeHours");
+        }
+    }
+}
