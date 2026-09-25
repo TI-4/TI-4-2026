fd126d0582e0eb7791431b45f1e8f6ce5d658909
Lukas Trillat <lstrillat@gmail.com>
2026-09-25 12:54:45 -0300

    fix(incident): remove lingering MongoDB import from ITicketRepository

--- src/Services/Incident/Incident.Domain/Repositories/ITicketRepository.cs
+++ src/Services/Incident/Incident.Domain/Repositories/ITicketRepository.cs
@@ -1,6 +1,6 @@
 using Incident.Domain.Entities;
-using MongoDB.Driver;
 namespace Incident.Domain.Repositories;
 public interface ITicketRepository : IRepository<Ticket>{
     //Es posible añadir mas metodos aqui
 }
+
